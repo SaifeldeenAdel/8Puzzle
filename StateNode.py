2e721341c24384860187594b35a86260e39bf23e
0marsaad <omar003saad@gmail.com>
2024-08-07 16:34:09 +0300

    get_neighbors method done

--- StateNode.py
+++ StateNode.py
@@ -15,7 +15,7 @@ class StateNode:
         
         return np.array([int(letter) for letter in str(self.state)]).reshape(3, 3)
 
-    def validate_state(self):
+    def validate_state(self) -> bool:
         ls = [0] * 9
         temp = self.state
         i = 0
@@ -30,8 +30,51 @@ class StateNode:
             return False
         else: return True
 
+    def __swap(self,string, i: int, j: int) -> str:
+
+        str_state = list(string)
+        str_state[i], str_state[j] = str_state[j], str_state[i]
+        return str(str_state)
 
 
-    def get_neighbors(self):
-        pass
+
+    def get_neighbors(self) -> list[StateNode]:
+        neighbors = []
+        temp = str(self.state)
+        i = temp.index('0')
+        if i == 0:
+            neighbors.append(StateNode(int(self.__swap(temp, i, i+1)), self))
+            neighbors.append(StateNode(int(self.__swap(temp, i, i+3)), self))
+        elif i == 1:
+            neighbors.append(StateNode(int(self.__swap(temp, i, i+1)), self))
+            neighbors.append(StateNode(int(self.__swap(temp, i, i+3)), self))
+            neighbors.append(StateNode(int(self.__swap(temp, i, i-1)), self))
+        elif i == 2:
+            neighbors.append(StateNode(int(self.__swap(temp, i, i+3)), self))
+            neighbors.append(StateNode(int(self.__swap(temp, i, i-1)), self))
+        elif i == 3:
+            neighbors.append(StateNode(int(self.__swap(temp, i, i+1)), self))
+            neighbors.append(StateNode(int(self.__swap(temp, i, i+3)), self))
+            neighbors.append(StateNode(int(self.__swap(temp, i, i-3)), self))
+        elif i == 4:
+            neighbors.append(StateNode(int(self.__swap(temp, i, i+1)), self))
+            neighbors.append(StateNode(int(self.__swap(temp, i, i+3)), self))
+            neighbors.append(StateNode(int(self.__swap(temp, i, i-3)), self))
+            neighbors.append(StateNode(int(self.__swap(temp, i, i-1)), self))
+        elif i == 5:
+            neighbors.append(StateNode(int(self.__swap(temp, i, i-1)), self))
+            neighbors.append(StateNode(int(self.__swap(temp, i, i+3)), self))
+            neighbors.append(StateNode(int(self.__swap(temp, i, i-3)), self))
+        elif i == 6:
+            neighbors.append(StateNode(int(self.__swap(temp, i, i+1)), self))
+            neighbors.append(StateNode(int(self.__swap(temp, i, i-3)), self))
+        elif i == 7:
+            neighbors.append(StateNode(int(self.__swap(temp, i, i+1)), self))
+            neighbors.append(StateNode(int(self.__swap(temp, i, i-3)), self))
+            neighbors.append(StateNode(int(self.__swap(temp, i, i-1)), self))
+        elif i == 8:
+            neighbors.append(StateNode(int(self.__swap(temp, i, i-1)), self))
+            neighbors.append(StateNode(int(self.__swap(temp, i, i-3)), self))
+        return neighbors
+        
 
